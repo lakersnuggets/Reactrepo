82e8bf0bfb1bb0ef691f4a089607a7042c194e23
Sourabh Vishwakarma <124355272+lakersnuggets@users.noreply.github.com>
2024-09-03 23:28:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import Header from './Header'
+import Footer from './Footer'
+import Digitalclock from './Digitalclock';
+
+function App() {
+  
+  return(
+    <>
+    <Header/>
+    <Footer/>
+    <Digitalclock/>
+    
+   
+    
+    </>
+  );
+}
+
+export default App
---
+++ Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+  return(
+
+    <header>
+      <h1>My Clock'O</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Alarm</a></li>
+          <li><a href="#">Timer</a></li>
+          <li><a href="#">Stopwatch</a></li>
+          <li><a href="#">Lap</a></li>
+        </ul>
+      </nav>
+      <hr></hr>
+    </header>
+
+  );
+}
+
+export default Header
